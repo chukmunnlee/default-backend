6777884c0accc9d809c66b791b971e22bd8a4364
Chuk <chukmunnlee@gmail.com>
2021-08-29 13:39:08 +0800

    ADDED: Support for HTML and JSON media type

--- main.js
+++ main.js
@@ -3,6 +3,21 @@ const express = require('express')
 
 const PORT = parseInt(process.env.PORT || process.env.APP_PORT) || 3000
 
+const return404 = (req, resp) => {
+	resp.status(404)
+	resp.format({
+		'text/html': () => {
+			resp.type('text/html').sendFile(__dirname + '/public/index.html')
+		},
+		'application/json': () => {
+			resp.type('application/json').json({ error: `Not found: ${req.originalUrl}` })
+		},
+		default: () => {
+			resp.type('text/plain').send(`Not found: ${req.originalUrl}`)
+		}
+	})
+}
+
 const app = express();
 
 app.disable('x-powered-by')
@@ -15,15 +30,11 @@ app.get('/healthz', (req, resp) => {
 		.json({ timestamp: (new Date()).getTime() })
 })
 
-app.get([ '/', '/index.html' ], (req, resp) => {
-	resp.status(404).type('text/html').sendFile(__dirname + '/public/index.html')
-})
+app.get([ '/', '/index.html' ], return404)
 
 app.use(express.static(__dirname + '/public'))
 
-app.use((req, resp) => {
-	resp.status(404).type('text/html').sendFile(__dirname + '/public/index.html')
-})
+app.use(return404)
 
 app.listen(PORT, () => {
 	console.info(`Application started on port ${PORT} at ${new Date()}`)
