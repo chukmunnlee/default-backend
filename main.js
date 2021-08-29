9e1133bcdf110c0e5d2ded5482f384dcc8b716eb
Chuk <chukmunnlee@gmail.com>
2021-08-29 13:16:51 +0800

    FIXED: return 404

--- main.js
+++ main.js
@@ -7,13 +7,23 @@ const app = express();
 
 app.disable('x-powered-by')
 
+app.use(morgan('combined'))
+
 app.get('/healthz', (req, resp) => {
 	resp.status(200)
 		.type('application/json')
 		.json({ timestamp: (new Date()).getTime() })
 })
 
+app.get([ '/', '/index.html' ], (req, resp) => {
+	resp.status(404).type('text/html').sendFile(__dirname + '/public/index.html')
+})
+
 app.use(express.static(__dirname + '/public'))
+
+app.use((req, resp) => {
+	resp.status(404).type('text/html').sendFile(__dirname + '/public/index.html')
+})
 
 app.listen(PORT, () => {
 	console.info(`Application started on port ${PORT} at ${new Date()}`)
